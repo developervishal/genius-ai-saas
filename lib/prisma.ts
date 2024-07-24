731f9367f179cd63115f1203d43a1505d013b5d1
Vishal Shrivastava <s96viky@gmail.com>
2024-07-24 20:02:53 +0530

    updating prisma ref

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,15 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-declare global {
-    var prisma: PrismaClient;
-}
-let prisma: PrismaClient;      
-
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
-} else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient();
-    }
-    prisma = global.prisma;
+const prismaClientSingleton = () => {
+    return new PrismaClient()
 }
 
-export default prisma;
+declare const globalThis: {
+    prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
